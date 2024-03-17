b33701fd19a3ea3fe4b1a23455c7a61d1e91ddbd
Przemyslaw T <gax36169@student.gdynia.merito.pl>
2024-03-17 12:59:40 +0100

    Pierwsza Klasa

---
+++ src/Film.java
@@ -0,0 +1,10 @@
+public class Film {
+    String name;
+    int year;
+
+    public void info() {
+        System.out.println(name+ " " + year);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Film movie = new Film();
+        movie.name = "Avatar";
+        movie.year= 2137;
+        movie.info();
+
+    }
+}
